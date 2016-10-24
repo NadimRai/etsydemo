fff75ed58235e13138e79f1b117dbea616dd7e48
NadimRai <rai_nadim@yahoo.com>
2016-10-23 20:40:39 -0700

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,6 @@ validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 validates :name, :description, :price, presence: true
 validates :price, numericality: {greater_than: 0}
 validates_attachment_presence :image
+
+belongs_to :user
 end
